c95fc07726aae42ee19d0cfe67c0ce6a52150343
krimtom <kmt3@hi.is>
2018-04-03 23:28:25 +0000

    Útfærsla á að eyða notanda sem admin, ásamt lagfæringu á að eyða auglýsingu sem admin

--- app/src/main/java/is/hi/teymi9/gefins/AdminDeleteUserFragment.java
+++ app/src/main/java/is/hi/teymi9/gefins/AdminDeleteUserFragment.java
@@ -1,11 +1,14 @@
 package is.hi.teymi9.gefins;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
@@ -23,7 +26,11 @@ import is.hi.teymi9.gefins.model.User;
 public class AdminDeleteUserFragment extends Fragment {
 
     private ListView mUserList;
-    private List<User> users;
+    private List<User> allUsers;
+    //Tilbaka takki
+    private Button mBack;
+    //Takki til að eyða auglýsingum
+    private Button mDelete;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,27 +42,72 @@ public class AdminDeleteUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_admin_delete_user, container, false);
-
+        mBack = (Button) v.findViewById(R.id.back_button2);
+        mDelete = (Button) v.findViewById(R.id.delete_ad_button3);
         mUserList = (ListView) v.findViewById(R.id.userList);
 
-        users = AdminActivity.getUserService().getAllUsers();
-        int count = users.size();
-        if(!users.isEmpty()){
-            String[] userName = new String[count];
-            int i = 0;
-            for(User u: users){
-                userName[i] = u.getUsername();
-                i++;
-            }
 
-            ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    android.R.layout.simple_list_item_1,
-                    userName
-            );
+        allUsers = AdminActivity.getUserService().getAllUsers();
+        int countUsers = allUsers.size();
 
-            mUserList.setAdapter(listViewAdapter);
+        //Býr til tóm strengjafylki að sömu stærð og fjöldi notenda
+        String[] username = new String[countUsers];
+        String[] fullName = new String[countUsers];
+        String[] email = new String[countUsers];
+        String[] phonenr = new String[countUsers];
+        Integer[] zip = new Integer[countUsers];
+        String[] address = new String[countUsers];
+        Boolean[] hasadminauthority = new Boolean[countUsers];
+
+        int i = 0;
+        for(User a: allUsers) {
+            //Fylla inn í strengjafylkin
+            username[i] = a.getUsername();
+            fullName[i] = a.getFullName();
+            email[i] = a.getEmail();
+            phonenr[i] = a.getPhonenr();
+            zip[i] = a.getZip();
+            address[i] = a.getAddress();
+            hasadminauthority[i] = a.isHasadminauthority();
+            i++;
         }
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_multiple_choice,
+                username
+        );
+
+        mUserList.setAdapter(listViewAdapter);
+
+
+        //Sendir admin tilbaka á admin síðu
+        mBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                User currentUser = LoginActivity.getUserService().getCurrentUser();
+                System.out.println("DisplayAdsFragment í back hnappi: currentUser er " + currentUser);
+                Intent intent = new Intent(getActivity(), AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray checked = mUserList.getCheckedItemPositions();
+                int size = checked.size(); // number of name-value pairs in the array
+                for (int i = 0; i < size; i++) {
+                    int key = checked.keyAt(i);
+                    boolean value = checked.get(key);
+                    if (value)
+                        System.out.println(key);
+                    String tag = mUserList.getItemAtPosition(key).toString();
+                    System.out.println(tag);
+                    AdminActivity.getAdService().deleteAdByName(tag, getActivity());
+                }
+            }
+        });
 
         return v;
     }
